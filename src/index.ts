45c6c26ba45be30fb057e42efb80fa5a17ccf535
Tankou <117810219+KoseiTanno@users.noreply.github.com>
2026-08-14 04:33:46 +0900

    feat(cli): add command line interface with exit codes (#17)
    Co-authored-by: KoseiTanno <KoseiTanno@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export {}
+export { lint } from './linter.js'
+export { rules } from './rules/index.js'
+export type {
+  Diagnostic,
+  Rule,
+  RuleContext,
+  RulesAST,
+  Severity,
+} from './types.js'
